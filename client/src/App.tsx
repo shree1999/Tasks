12728d632b1e102b7b0d9888ca188212019c0a87
Shreeansh <sarojvarshney20@gmail.com>
2021-05-14 17:47:16 +0530

    add create task

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,85 @@
+import { useState, useEffect, FormEvent } from 'react';
 import axios from 'axios';
 
-interface ISuccess {
+interface ITask {
+  title: string;
+  _id: string;
+  _v: string;
+}
+
+interface IErrorResponse {
   success: boolean;
   message: string;
 }
 
+interface IResponse {
+  success: boolean;
+  tasks: Array<ITask>;
+}
+
 export const App: React.FC = () => {
-  const onClickHandler = async () => {
+  const [taskData, setTaskData] = useState<IResponse | null>(null);
+  const [error, setError] = useState<IErrorResponse | null>(null);
+  const [task, setTask] = useState<string>('');
+
+  const loadTasks = async () => {
     try {
-      const res = await axios.get<ISuccess>('/api/');
-      console.log(res.data);
+      const res = await axios.get<IResponse>('/api/');
+      setTaskData(res.data);
+      setError(null);
     } catch (err) {
-      console.error(err);
+      console.error(err.response);
+      setError(err.response.data);
+    }
+  };
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  const onSubmitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post('/api/', { title: task });
+      console.log(res.data);
+      loadTasks();
+    } catch (err) {
+      setError(err.response.data);
     }
   };
 
   return (
     <div>
-      <h1>Hello from React</h1>
-      <button onClick={onClickHandler}>Talk to server</button>
+      <div className="container-fluid">
+        <h1 className="display-2">Task-Creator</h1>
+        <form onSubmit={onSubmitHandler}>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Task"
+              value={task}
+              onChange={e => setTask(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+
+        <h2 className="display-3">Tasks Created: </h2>
+
+        {error && <div className="alert alert-danger">{error.message}</div>}
+
+        {taskData?.success &&
+          taskData.tasks.length > 0 &&
+          taskData.tasks.map(t => (
+            <div className="alert alert-secondary" key={t._id}>
+              {t.title}
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
